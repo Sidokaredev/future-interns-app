59b5d979b5d71c6f794c53f06d35b734180119ef
barjakoub <oujakdev.rep@gmail.com>
2025-06-10 03:19:50 +0700

    fix: build result vite basepath

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/future-interns-app/',
+  base: '/future-interns-app/',
   plugins: [react()],
   server: {
     headers: {
